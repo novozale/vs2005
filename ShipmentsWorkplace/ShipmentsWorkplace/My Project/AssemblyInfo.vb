657aa41d38d8dee4402e3c4d5769673112e10725
Alexander <novozale@mail.ru>
2024-01-11 16:34:07 +0300

    Add files via upload

---
+++ ShipmentsWorkplace/ShipmentsWorkplace/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ShipmentsWorkplace")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Elektroskandia")> 
+<Assembly: AssemblyProduct("ShipmentsWorkplace")> 
+<Assembly: AssemblyCopyright("Copyright © Elektroskandia 2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f98de902-c8bb-4141-8edc-a0e13470aeaa")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
